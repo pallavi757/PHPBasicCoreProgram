400fc35001d6d1fec24da56ff798df243b0ef3ee
unknown <kinekarpallavi21@gmail.com>
2022-05-20 23:33:57 +0530

    [Add Pallavi]To check enterd alphabet is vowel or consonant using in_array()

---
+++ vowelcosonant.php
@@ -0,0 +1,13 @@
+<?php 
+    $vowels = ['a','e','i','o','u'];//declare static array
+    $letter = readLine("Enter a character: ");//read character from user using readLine() and stored in letter varible
+
+    if (in_array(strtolower($letter), $vowels)) 
+    {
+        echo $letter." is a Vowel";
+    }
+     else 
+     {
+        echo $letter." is a Consonant";
+     }
+    ?> 
